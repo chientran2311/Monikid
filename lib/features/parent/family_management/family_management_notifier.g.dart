d4eb616e2ad6452ccb50083e82d7bc2c97c68efe
chientran2311 <chien2977@gmail.com>
2026-05-16 00:15:30 +0700

    build: generate Freezed and Riverpod files for family_management
    Auto-generated by build_runner after adding FamilyManagementState
    and FamilyManagementNotifier.
    
    Part of Phase 4.1 Plan 04.1-01.

---
+++ lib/features/parent/family_management/family_management_notifier.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'family_management_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$familyManagementNotifierHash() =>
+    r'616346626ae4edc43f5f88c66e4e6be47d632f06';
+
+/// See also [FamilyManagementNotifier].
+@ProviderFor(FamilyManagementNotifier)
+final familyManagementNotifierProvider =
+    AutoDisposeNotifierProvider<
+      FamilyManagementNotifier,
+      FamilyManagementState
+    >.internal(
+      FamilyManagementNotifier.new,
+      name: r'familyManagementNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$familyManagementNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FamilyManagementNotifier = AutoDisposeNotifier<FamilyManagementState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
